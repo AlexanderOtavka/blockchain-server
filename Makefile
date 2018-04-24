74b413f76b43a07d3e1508f7e01349fd90a33b29
Alissa R. Johnson <johnsona5@cs.grinnell.edu>
2018-04-24 15:53:05 -0500

    Added http parsing.

--- Makefile
+++ Makefile
@@ -1,11 +1,18 @@
 CC = clang
 
-CFLAGS = -g `pkg-config --libs openssl`
+CFLAGS = -g
+SSLFLAGS = `pkg-config --libs openssl`
 
 all: server
 
-server: server.c
-	$(CC) $(CFLAGS) -o server server.c
+server: server.o parser.o
+	$(CC) $(CFLAGS) $(SSLFLAGS) -o server server.o parser.o
+
+server.o: server.c
+	$(CC) $(CFLAGS) -c -o server.o server.c
+
+parser.o: picohttpparser/picohttpparser.c
+	$(CC) $(CFLAGS) -c -o parser.o picohttpparser/picohttpparser.c
 
 clean:
 	rm -f server
--- server.c
+++ server.c
@@ -5,122 +5,177 @@
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 #include <string.h>
+#include <assert.h>
+
+#include "picohttpparser/picohttpparser.h"
+
+int handle_request (SSL* ssl) {
+  char buf[4096];
+  const char *method, *path;
+  int pret, minor_version;
+  struct phr_header headers[100];
+  size_t buflen = 0, prevbuflen = 0, method_len, path_len, num_headers;
+  ssize_t rret;
+
+  while (1) {
+    /* read the request */
+    while ((rret = SSL_read(ssl, buf + buflen, sizeof(buf) - buflen)) == -1
+           && errno == EINTR);
+    if (rret <= 0)
+      return -1;
+    prevbuflen = buflen;
+    buflen += rret;
+    /* parse the request */
+    num_headers = sizeof(headers) / sizeof(headers[0]);
+    pret = phr_parse_request(buf, buflen, &method, &method_len, &path,
+                             &path_len, &minor_version, headers, &num_headers,
+                             prevbuflen);
+    if (pret > 0)
+      break; /* successfully parsed the request */
+    else if (pret == -1)
+      return -1;
+    /* request is incomplete, continue the loop */
+    assert(pret == -2);
+    if (buflen == sizeof(buf))
+      return -1;
+  }
+
+  printf("request is %d bytes long\n", pret);
+  printf("method is %.*s\n", (int)method_len, method);
+  printf("path is %.*s\n", (int)path_len, path);
+  printf("HTTP version is 1.%d\n", minor_version);
+  printf("headers:\n");
+  for (int i = 0; i != num_headers; ++i) {
+    printf("%.*s: %.*s\n", (int)headers[i].name_len, headers[i].name,
+           (int)headers[i].value_len, headers[i].value);
+  }
+
+  char * response = "HTTP/1.1 404 Not Found\r\n"
+    "Content-Type: text/plain\r\n"
+    "Content-Length: 4\r\n"
+    "\r\n"
+    "not found\r\n"
+    ;
+  SSL_write(ssl, response, strlen(response));
+  return 0;
+}
 
 int create_socket(int port)
 {
-    int s;
-    struct sockaddr_in addr;
+  int s;
+  struct sockaddr_in addr;
 
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(port);
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    s = socket(AF_INET, SOCK_STREAM, 0);
-    if (s < 0) {
-	perror("Unable to create socket");
-	exit(EXIT_FAILURE);
-    }
+  s = socket(AF_INET, SOCK_STREAM, 0);
+  if (s < 0) {
+    perror("Unable to create socket");
+    exit(EXIT_FAILURE);
+  }
 
-    if (bind(s, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-	perror("Unable to bind");
-	exit(EXIT_FAILURE);
-    }
+  if (bind(s, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+    perror("Unable to bind");
+    exit(EXIT_FAILURE);
+  }
 
-    if (listen(s, 1) < 0) {
-	perror("Unable to listen");
-	exit(EXIT_FAILURE);
-    }
+  if (listen(s, 1) < 0) {
+    perror("Unable to listen");
+    exit(EXIT_FAILURE);
+  }
 
-    return s;
+  return s;
 }
 
 void init_openssl()
 { 
-    SSL_load_error_strings();	
-    OpenSSL_add_ssl_algorithms();
+  SSL_load_error_strings();	
+  OpenSSL_add_ssl_algorithms();
 }
 
 void cleanup_openssl()
 {
-    EVP_cleanup();
+  EVP_cleanup();
 }
 
 SSL_CTX *create_context()
 {
-    const SSL_METHOD *method;
-    SSL_CTX *ctx;
+  const SSL_METHOD *method;
+  SSL_CTX *ctx;
 
-    method = TLS_server_method();
+  method = TLS_server_method();
 
-    ctx = SSL_CTX_new(method);
+  ctx = SSL_CTX_new(method);
 
-    SSL_CTX_set_min_proto_version(ctx, TLS1_2_VERSION);
+  //SSL_CTX_set_min_proto_version(ctx, TLS1_2_VERSION);
     
-    if (!ctx) {
-	perror("Unable to create SSL context");
-	ERR_print_errors_fp(stderr);
-	exit(EXIT_FAILURE);
-    }
+  if (!ctx) {
+    perror("Unable to create SSL context");
+    ERR_print_errors_fp(stderr);
+    exit(EXIT_FAILURE);
+  }
 
-    return ctx;
+  return ctx;
 }
 
 void configure_context(SSL_CTX *ctx)
 {
-    SSL_CTX_set_ecdh_auto(ctx, 1);
+  SSL_CTX_set_ecdh_auto(ctx, 1);
 
-    /* Set the key and cert */
-    if (SSL_CTX_use_certificate_file(ctx, "cert.pem", SSL_FILETYPE_PEM) <= 0) {
-        ERR_print_errors_fp(stderr);
-	exit(EXIT_FAILURE);
-    }
+  /* Set the key and cert */
+  if (SSL_CTX_use_certificate_file(ctx, "cert.pem", SSL_FILETYPE_PEM) <= 0) {
+    ERR_print_errors_fp(stderr);
+    exit(EXIT_FAILURE);
+  }
 
-    if (SSL_CTX_use_PrivateKey_file(ctx, "key.pem", SSL_FILETYPE_PEM) <= 0 ) {
-        ERR_print_errors_fp(stderr);
-	exit(EXIT_FAILURE);
-    }
+  if (SSL_CTX_use_PrivateKey_file(ctx, "key.pem", SSL_FILETYPE_PEM) <= 0 ) {
+    ERR_print_errors_fp(stderr);
+    exit(EXIT_FAILURE);
+  }
 }
 
 int main(int argc, char **argv)
 {
-    int sock;
-    SSL_CTX *ctx;
+  int sock;
+  SSL_CTX *ctx;
 
-    init_openssl();
-    ctx = create_context();
+  init_openssl();
+  ctx = create_context();
 
-    configure_context(ctx);
+  configure_context(ctx);
 
-    sock = create_socket(4433);
+  sock = create_socket(4433);
 
-    /* Handle connections */
-    while(1) {
-        struct sockaddr_in addr;
-        uint len = sizeof(addr);
-        SSL *ssl;
-        const char reply[] = "test\n";
+  /* Handle connections */
+  while(1) {
+    struct sockaddr_in addr;
+    uint len = sizeof(addr);
+    SSL *ssl;
 
-        int client = accept(sock, (struct sockaddr*)&addr, &len);
-        if (client < 0) {
-            perror("Unable to accept");
-            exit(EXIT_FAILURE);
-        }
-
-        ssl = SSL_new(ctx);
-        SSL_set_fd(ssl, client);
-
-        if (SSL_accept(ssl) <= 0) {
-            ERR_print_errors_fp(stderr);
-        }
-        else {
-            SSL_write(ssl, reply, strlen(reply));
-        }
-
-        SSL_free(ssl);
-        close(client);
+    int client = accept(sock, (struct sockaddr*)&addr, &len);
+    if (client < 0) {
+      perror("Unable to accept");
+      exit(EXIT_FAILURE);
     }
 
-    close(sock);
-    SSL_CTX_free(ctx);
-    cleanup_openssl();
+    ssl = SSL_new(ctx);
+    SSL_set_fd(ssl, client);
+
+    if (SSL_accept(ssl) <= 0) {
+      ERR_print_errors_fp(stderr);
+    }
+    else {
+      handle_request(ssl);
+      
+    }
+
+        
+    SSL_free(ssl);
+    close(client);
+  }
+
+  close(sock);
+  SSL_CTX_free(ctx);
+  cleanup_openssl();
 }
